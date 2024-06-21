abeb184f085cf1c16f7554dbf5215058fe1b8900
LukasDeco <ldeconantsesznak@gmail.com>
2024-06-21 13:24:49 -0600

    fix: update token_accts on watch

--- src/services/transactions.rs
+++ src/services/transactions.rs
@@ -8,12 +8,13 @@ use diesel::PgConnection;
 
 use crate::entities::token_acct_balances::token_acct_balances;
 use crate::entities::token_acct_balances::TokenAcctBalances;
+use crate::entities::token_accts::token_accts;
 // use crate::entrypoints::events;
 
 /**
  * Handles updating our DB for a tx that affects a token acct balance.
+ * Will update both token_accts and token_acct_balances table with the new balance amount
  */
-//TODO: create status Watching for updates on tx
 pub async fn handle_token_acct_balance_tx(
     conn_manager: Arc<Object<Manager<PgConnection>>>,
     token_acct: String,
@@ -25,7 +26,7 @@ pub async fn handle_token_acct_balance_tx(
 ) -> Result<(), Box<dyn std::error::Error>> {
     // Query the most recent value for the given token_acct to calculate the delta
     let token_acct_clone_1 = token_acct.clone();
-    let previous_balance = conn_manager
+    let previous_balance: Option<_> = conn_manager
         .interact(move |db| {
             token_acct_balances::table
                 .filter(token_acct_balances::token_acct.eq(token_acct_clone_1))
@@ -55,7 +56,6 @@ pub async fn handle_token_acct_balance_tx(
         .await?;
 
     let maybe_balance = existing_balance_res.ok();
-    let maybe_balance_copy = maybe_balance.clone();
 
     if let Some(balance) = maybe_balance {
         if balance.tx_sig.is_none() {
@@ -75,9 +75,8 @@ pub async fn handle_token_acct_balance_tx(
                 })
                 .await??;
         }
-    }
-
-    if maybe_balance_copy.is_none() {
+        // already has the correct tx_sig, no need to update anything
+    } else {
         let new_token_acct_balance = TokenAcctBalances {
             token_acct: token_acct.clone(),
             mint_acct: mint_acct,
@@ -97,6 +96,18 @@ pub async fn handle_token_acct_balance_tx(
             })
             .await??;
     }
+
+    // Update the token_accts table with the new balance in the amount column
+    let token_acct_clone_4 = token_acct.clone();
+    conn_manager
+        .interact(move |db| {
+            diesel::update(
+                token_accts::table.filter(token_accts::token_acct.eq(token_acct_clone_4)),
+            )
+            .set(token_accts::amount.eq(new_balance))
+            .execute(db)
+        })
+        .await??;
 
     Ok(())
 }
