9c85433a8a3bfdcc103f3149bb8ae929e557323e
kilgaloon <radju22@gmail.com>
2024-08-14 15:43:00 +0200

    Store user deposits

--- src/services/deposits.rs
+++ src/services/deposits.rs
@@ -1,9 +1,9 @@
-use crate::entities::{transactions::Instruction, deposits::UserDeposit};
 use crate::entities::deposits::user_deposits;
+use crate::entities::{deposits::UserDeposit, transactions::Instruction};
 use chrono::Utc;
-use diesel::{PgConnection, RunQueryDsl};
 use deadpool::managed::Object;
 use deadpool_diesel::Manager;
+use diesel::{PgConnection, RunQueryDsl};
 use std::sync::Arc;
 
 pub async fn handle_deposit(
@@ -13,26 +13,22 @@ pub async fn handle_deposit(
     mint_instruction: &Instruction,
     mint_acct: String,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    let amount: i64 = mint_instruction
+        .args
+        .iter() // Create an iterator over the arguments
+        .find(|arg| arg.name == "amount") // Find the argument with the name "amount"
+        .and_then(|arg| arg.data.parse().ok()) // Parse the data
+        .unwrap_or(0); // Handle the case where the argument is not found
 
-    let amount: i64 = mint_instruction.args
-    .iter() // Create an iterator over the arguments
-    .find(|arg| arg.name == "amount") // Find the argument with the name "amount"
-    .and_then(|arg| arg.data.parse().ok()) // Parse the data
-    .unwrap_or(0); // Handle the case where the argument is not found
+    let deposit = UserDeposit::new(authority_account, amount, mint_acct, tx_sig);
 
-    let deposit = UserDeposit::new(
-        authority_account, 
-        amount, 
-        mint_acct, 
-        tx_sig,
-    );
-    
     conn_manager
-    .interact(move |db| {
-        diesel::insert_into(user_deposits::table)
-            .values(&deposit)
-            .execute(db)
-    }).await??;
+        .interact(move |db| {
+            diesel::insert_into(user_deposits::table)
+                .values(&deposit)
+                .execute(db)
+        })
+        .await??;
 
     Ok(())
 }
