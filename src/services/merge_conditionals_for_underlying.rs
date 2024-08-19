66a00f58f1afd61adf263bb6bda63fb7d576a4c2
LukasDeco <ldeconantsesznak@gmail.com>
2024-08-19 15:02:04 -0600

    fix: post rebase compiler bugs

--- src/services/merge_conditionals_for_underlying.rs
+++ src/services/merge_conditionals_for_underlying.rs
@@ -5,21 +5,26 @@ use deadpool::managed::Object;
 use deadpool_diesel::Manager;
 use diesel::PgConnection;
 
-use super::{balances,transactions};
+use super::{balances, transactions};
 
 pub async fn handle_merge_conditional_tokens_tx(
     conn_manager: Arc<Object<Manager<PgConnection>>>,
     transaction_payload: &Payload,
     transaction_sig: String,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let mint_instruction = transactions::find_instruction(&transaction_payload, "mergeConditionalTokensForUnderlyingTokens")?;
+    let mint_instruction = transactions::find_instruction(
+        &transaction_payload,
+        "mergeConditionalTokensForUnderlyingTokens",
+    )?;
     let authority_account = transactions::find_authority_account(&mint_instruction)?;
     let vault_account = transactions::find_vault_account(&mint_instruction)?;
     let conditional_vault =
         transactions::get_conditional_vault(Arc::clone(&conn_manager), &vault_account).await?;
 
-    let relevant_accounts =
-        transactions::get_relevant_accounts_from_mint_and_vault(&mint_instruction, conditional_vault);
+    let relevant_accounts = transactions::get_relevant_accounts_from_mint_and_vault(
+        &mint_instruction,
+        &conditional_vault,
+    );
 
     for (token_account, mint_acct_value) in &relevant_accounts {
         balances::handle_token_acct_in_tx(
--- src/services/redeem_conditionals.rs
+++ src/services/redeem_conditionals.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use super::balances;
+use super::transactions;
 use crate::entities::conditional_vaults::conditional_vaults::dsl::*;
 use crate::entities::conditional_vaults::ConditionalVault;
 use crate::entities::transactions::Instruction;
@@ -9,21 +10,25 @@ use deadpool::managed::Object;
 use deadpool_diesel::Manager;
 use diesel::prelude::*;
 use diesel::PgConnection;
-use super::transactions;
 
 pub async fn handle_redeem_conditional_tokens_tx(
     conn_manager: Arc<Object<Manager<PgConnection>>>,
     transaction_payload: &Payload,
     transaction_sig: String,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let mint_instruction = transactions::find_instruction(&transaction_payload, "redeemConditionalTokensForUnderlyingTokens")?;
+    let mint_instruction = transactions::find_instruction(
+        &transaction_payload,
+        "redeemConditionalTokensForUnderlyingTokens",
+    )?;
     let authority_account = transactions::find_authority_account(&mint_instruction)?;
     let vault_account = transactions::find_vault_account(&mint_instruction)?;
     let conditional_vault =
         transactions::get_conditional_vault(Arc::clone(&conn_manager), &vault_account).await?;
 
-    let relevant_accounts =
-       transactions::get_relevant_accounts_from_mint_and_vault(&mint_instruction, conditional_vault);
+    let relevant_accounts = transactions::get_relevant_accounts_from_mint_and_vault(
+        &mint_instruction,
+        &conditional_vault,
+    );
 
     for (token_account, mint_acct_value) in &relevant_accounts {
         balances::handle_token_acct_in_tx(
